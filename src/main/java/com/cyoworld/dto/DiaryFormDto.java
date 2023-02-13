21a0b10f77c97ab8924c1b7df25f7610eb434d59
geniigrace <jeyeom10@naver.com>
2023-02-14 00:32:07 +0900

    다이어리 : 작성시 필요한 DTO 추가

--- src/main/java/com/cyoworld/dto/DiaryFormDto.java
+++ src/main/java/com/cyoworld/dto/DiaryFormDto.java
@@ -1,4 +1,14 @@
 package com.cyoworld.dto;
 
 public class DiaryFormDto {
+
+   private Long id;
+
+   private String diaryTitle;
+
+   private String diaryContent;
+
+   private String mapKeyword;
+
+
 }
