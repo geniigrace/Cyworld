437491ad6253c3db7e70a39c903d61ef757e90f9
saewoone <121207816+saewoone@users.noreply.github.com>
2023-02-15 21:04:32 +0900

    로그인 안 됨

--- src/main/java/com/cyoworld/controller/SettingController.java
+++ src/main/java/com/cyoworld/controller/SettingController.java
@@ -1,18 +1,63 @@
 package com.cyoworld.controller;
 
+import com.cyoworld.Service.SettingService;
+import com.cyoworld.constant.SettingStatus;
+import com.cyoworld.dto.SettingManageMenuDto;
+import com.cyoworld.entity.SettingMenu;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/setting")
 public class SettingController {
+    private final SettingService settingService;
 
-    @GetMapping(value = "")
-    public String settingBoard(){
+    @GetMapping(value = "/menu")
+    public String settingMenu(Model model, Principal principal){
+//        SettingMenu settingMenu = settingService.findSettingMenuByUserId(principal.getName());
+//
+//        SettingManageMenuDto smmd = new SettingManageMenuDto();
+//        smmd.setId(settingMenu.getId());
+//        smmd.setAlbumPhoto(settingMenu.getAlbumPhoto());
+//        smmd.setAlbumVideo(settingMenu.getAlbumVideo());
+//        smmd.setBoard(settingMenu.getBoard());
+//        smmd.setDiary(settingMenu.getDiary());
+//        smmd.setGuestbook(settingMenu.getGuestbook());
+//        smmd.setBookmark(settingMenu.getBookmark());
+//        model.addAttribute("settingManageMenuDto", smmd);
 
-        return "setting/settings";
+//        model.addAttribute("settingManageMenuDto", new SettingManageMenuDto());
+
+        SettingManageMenuDto smmd = new SettingManageMenuDto();
+        smmd.setAlbumPhoto(SettingStatus.ON);
+        smmd.setAlbumVideo(SettingStatus.ON);
+        smmd.setBoard(SettingStatus.OFF);
+        smmd.setDiary(SettingStatus.OFF);
+        smmd.setGuestbook(SettingStatus.ON);
+        smmd.setBookmark(SettingStatus.ON);
+        model.addAttribute("settingManageMenuDto", smmd);
+
+        return "setting/settingMenu";
+    }
+
+    @PostMapping(value = "/saveMenuSetting")
+    public String saveMenuSetting(SettingManageMenuDto settingManageMenuDto, Principal principal){
+        SettingMenu settingMenu = settingService.findSettingMenuByUserId(principal.getName());
+        settingMenu.updateSettingMenu(settingManageMenuDto);
+
+        return "redirect:/setting/menu";
+    }
+
+    @GetMapping(value = "/blocked")
+    public String settingBlocked(Model model, Principal principal){
+
+        return "setting/settingBlocked";
     }
 }
--- src/main/java/com/cyoworld/entity/Member.java
+++ src/main/java/com/cyoworld/entity/Member.java
@@ -1,4 +1,61 @@
 package com.cyoworld.entity;
 
+import com.cyoworld.constant.Gender;
+import com.cyoworld.dto.MemberFormDto;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "member")
+@Getter
+@Setter
+@ToString
 public class Member {
+    @Id
+    @Column(name = "userCode")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(unique = true)
+    private String userId;
+
+    private String password;
+
+    private String name;
+
+    //private String birthday;
+    private int birthdayYear;
+    private int birthdayMonth;
+    private int birthdayDay;
+
+    private String email;
+
+    private String address;
+
+    private Gender gender;
+
+    public static Member createMember(MemberFormDto memberFormDto, PasswordEncoder passwordEncoder){
+        Member member = new Member();
+        member.setUserId(memberFormDto.getUserId());
+        String password = passwordEncoder.encode(memberFormDto.getPassword());
+        member.setName(memberFormDto.getName());
+        member.setBirthdayYear(memberFormDto.getBirthdayYear());
+        member.setBirthdayMonth(memberFormDto.getBirthdayMonth());
+        member.setBirthdayDay(memberFormDto.getBirthdayDay());
+        member.setEmail(memberFormDto.getEmail());
+        member.setAddress(memberFormDto.getAddress());
+        member.setPassword(password);
+        member.setGender(memberFormDto.getGender());
+        return member;
+    }
+
+    //profileImg
+    //homeImg
+
+    //createMember할 때 new SettingMenu -> 기본값(전체 ON)으로 갖고있게
+
 }
