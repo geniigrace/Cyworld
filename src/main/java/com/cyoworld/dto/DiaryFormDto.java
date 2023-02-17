3f63a62c3d32d5a9986f6d83a89d19193125720f
geniigrace <jeyeom10@naver.com>
2023-02-17 18:37:20 +0900

    지은_2023/02/17_BaseTimeEntity 생성, 다이어리 작성 관련 Dto, Entity 생성, Form.html 생성

--- src/main/java/com/cyoworld/dto/DiaryFormDto.java
+++ src/main/java/com/cyoworld/dto/DiaryFormDto.java
@@ -1,15 +1,10 @@
 package com.cyoworld.dto;
 
 public class DiaryFormDto {
-   //구분자 
-   private Long id;
 
-   private String diaryTitle;
+   private Long diaryId;
 
    private String diaryContent;
-
-   //다이어리 작성시 지도에 연결할 키워드 dto
-   private String mapKeyword;
 
 
 }
--- src/main/java/com/cyoworld/entity/Diary.java
+++ src/main/java/com/cyoworld/entity/Diary.java
@@ -1,4 +1,32 @@
 package com.cyoworld.entity;
 
-public class Diary {
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="diary")
+@Getter
+@Setter
+@ToString
+public class Diary extends BaseTimeEntity{
+
+    @Id
+    @Column(name = "diary_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long diaryId;
+
+    @Column(name = "diary_emoji", nullable = false)
+    private String diaryEmoji;
+
+    @Lob
+    @Column(name = "diary_content", nullable = false)
+    private String diaryContent;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private Member member;
+
 }
