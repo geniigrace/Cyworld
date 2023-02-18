bccc51364a751c1558217c128acba40a4076344f
geniigrace <jeyeom10@naver.com>
2023-02-18 20:25:35 +0900

    지은_2023/02/18_다이어리 엔티티 멤버 주석처리 하였음

--- src/main/java/com/cyoworld/entity/Diary.java
+++ src/main/java/com/cyoworld/entity/Diary.java
@@ -25,8 +25,8 @@ public class Diary extends BaseTimeEntity{
     @Column(name = "diary_content", nullable = false)
     private String diaryContent;
 
-    @OneToMany(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
-    private Member member;
+//    @OneToMany(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "user_id")
+//    private Member member;
 
 }
